76618c0b8468598e4af1c75cc12e00a7d08bcd3c
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-11 17:52:03 +0700

    create business logic or method in aplikasitodolist.java

--- AplikasiTodoList.java
+++ AplikasiTodoList.java
@@ -4,4 +4,26 @@ public class AplikasiTodoList {
     public static void main(String[] args) {
         
     }
+
+    /**
+     * Menampilkan todo list
+     */
+    public static void showTodoList() {
+
+    }
+
+    /**
+     * Menambah todo ke list
+     */
+    public static void addTodoList() {
+
+    }
+
+    /**
+     * Menghapus todo dari list
+     */
+    public static void removeTodoList() {
+
+    }
+    
 }
